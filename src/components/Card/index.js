0175ed5afab1fdacaeb72ae6bf5fc3e46ff8334f
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-11-07 16:46:28 +0000

    add pokemon pics from api

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import './style.css'
 
-
 export const Card = ({ url, name, id }) => {
 
-
     return (
-        <div>
-
+        <div id='card-div'>
             <img src={url} alt={`pic of ${name}`} />
-
             <h5><span>#{id}-</span>{name}</h5>
-
-
-
-
         </div>
     )
 }
--- src/pages/genPokemon/index.js
+++ src/pages/genPokemon/index.js
@@ -1,17 +1,63 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import { Card } from '../../components'
+import './style.css'
 
 export const GenPokemon = () => {
 
-    // const [fetch, setFetch] = useState();
+    const [loading, setLoading] = useState('https://pokeapi.co/api/v2/pokemon?limit=10');
+    const [fetchPokemon, setFetchPokemon] = useState([]);
 
-    // useEffect()
+
+
+    const fetching = async () => {
+        try {
+            const { data } = await axios.get(loading)
+            setLoading(data.next)
+
+            const getPokemon = (results) => {
+                results.forEach(async (pokemon) => {
+                    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+
+                    setFetchPokemon(prevState => [...prevState, data])
+                })
+            }
+            getPokemon(data.results)
+
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+
+    useEffect(() => {
+        fetching()
+
+    }, [])
+
+    // const handleClick = async () => {
+
+    //     const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon-species')
+    //     console.log(data.results)
+    //     return (
+    //         data.results.map((name, url, i) => <Card name={name} url={url} key={`${i + 1}`} />)
+
+    //     )
+
+    // }
+
+    console.log(fetchPokemon)
+
     return (
-        <>
+        <div id='main-div'>
+
+            {fetchPokemon.map((p, i) => (<Card key={i} name={p.name} url={p.sprites.front_default} id={i + 1} />))}
+
+            {/* {fetchPokemon} */}
             <button>Generate</button>
-
-
-        </>
+        </div>
     )
 }
 
